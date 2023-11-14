98bfcbc17d71d82be3c79e80e410fd52a31b0f0e
Crowdbotics <team@crowdbotics.com>
2023-11-14 20:55:28 +0000

    Initial commit

---
+++ backend/skip_44528/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for skip_44528 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "skip_44528.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "skip-44528",
+  url: "https://skip-44528.botics.co",
+  api: "https://skip-44528.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
